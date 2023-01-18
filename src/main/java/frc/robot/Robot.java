b11844db7fea0f4d44f372e69cc0913e93e50c1b
Asha Singhal <asha.learning366@hotmail.com>
2023-01-17 19:11:47 -0500

    Enabled data logging and network tables/driver joystick data

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+
+
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -28,6 +32,11 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    DataLogManager.start();
+    // Enables network table logging for data 
+    DataLogManager.logNetworkTables(true);
+    // logs joystick data and driver data 
+    DriverStation.startDataLog(DataLogManager.getLog());
   }
 
   /**
@@ -99,5 +108,11 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically whilst in simulation. */
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {  
+    
+
+  }
+
+   
+   
 }
