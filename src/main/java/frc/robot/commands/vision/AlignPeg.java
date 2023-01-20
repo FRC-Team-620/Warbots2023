4d74510eb981c84d97eacfa7f63f3735c959d18b
m10653 <mcombsb1@gmail.com>
2023-01-20 00:20:31 +0000

    Implemented AlignPeg command

--- src/main/java/frc/robot/commands/vision/AlignPeg.java
+++ src/main/java/frc/robot/commands/vision/AlignPeg.java
@@ -6,41 +6,68 @@ package frc.robot.commands.vision;
 
 import frc.robot.PhotonManager;
 import frc.robot.subsystems.Drivetrain;
+import org.photonvision.common.hardware.VisionLEDMode;
+import org.photonvision.targeting.PhotonPipelineResult;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-/** An example command that uses an example subsystem. */
+/** Rotates the robot to align to a identified target. */
 public class AlignPeg extends CommandBase {
-  // @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final Drivetrain m_subsystem;
+  private final Drivetrain m_drivetrain;
+  private final double kp = 0.1, ki = 0, kd = 0;
+  private final double kangleTolerance=1;
+  private final double kmaxTurnSpeed=.5;
+  PIDController m_pid = new PIDController(kp, ki, kd);
 
   /**
-   * Creates a new ExampleCommand.
+   * Rotates the robot to align to a identified target.
    *
-   * @param subsystem The subsystem used by this command.
+   * @param drivetrain The subsystem used by this command.
    */
-  public AlignPeg(Drivetrain subsystem) {
-    m_subsystem = subsystem;
+  public AlignPeg(Drivetrain drivetrain) {
+    m_drivetrain = drivetrain;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
-    var t = PhotonManager.getInstance().mainCam;
+    addRequirements(drivetrain);
     
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    PhotonManager.getInstance().mainCam.setLED(VisionLEDMode.kOn);
+    PhotonManager.getInstance().mainCam.setPipelineIndex(0);
+    m_pid.reset();
+    m_pid.setTolerance(kangleTolerance);
+    m_pid.setSetpoint(0);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+     PhotonPipelineResult result = PhotonManager.getInstance().mainCam.getLatestResult();
+     if(!result.hasTargets()){ // Checks to see if there is a Vision target
+      return;
+     }
+
+     double targetYaw = result.getBestTarget().getYaw(); // Yaw in degrees from Center of camera + is Right
+     
+     double output = MathUtil.clamp( m_pid.calculate(targetYaw), -kmaxTurnSpeed, kmaxTurnSpeed); // Camp Turn output between kmaxTurnSpeed
+    
+     m_drivetrain.setCurvatureDrive(0, output, true); //Turn Robot
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    PhotonManager.getInstance().mainCam.setLED(VisionLEDMode.kOff);
+    // TODO: Reset to old Pipeline or driver mode?
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    // TODO: End cmd if the robot is at the setpoint.
     return false;
   }
 }
