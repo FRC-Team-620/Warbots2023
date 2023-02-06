7d8b2f4687d35c262c84b55a7501d29d090ec79a
Ava <transorsmth@gmail.com>
2023-02-06 09:54:00 -0500

    Fixed unnecessary import

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,8 +4,6 @@
 
 package frc.robot.subsystems;
 
-import org.apache.commons.lang3.NotImplementedException;
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
