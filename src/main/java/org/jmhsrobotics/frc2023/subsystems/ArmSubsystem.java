c6e2e5f97e76fc4c5691ede52717dfd6fb9b7590
m10653 <mcombsb1@gmail.com>
2023-02-09 01:09:22 +0000

    Added Provisional Arm Simulation support

--- src/main/java/org/jmhsrobotics/frc2023/subsystems/ArmSubsystem.java
+++ src/main/java/org/jmhsrobotics/frc2023/subsystems/ArmSubsystem.java
@@ -1,11 +1,19 @@
 package org.jmhsrobotics.frc2023.subsystems;
 
+import org.jmhsrobotics.frc2023.Constants;
+import org.jmhsrobotics.frc2023.util.sim.RevEncoderSimWrapper;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxAbsoluteEncoder;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
@@ -27,16 +35,17 @@ public class ArmSubsystem extends SubsystemBase {
 	public ProfiledPIDController profiledExtensionPID;
 
 	public ArmSubsystem() {
-		Mechanism2d mech = new Mechanism2d(3, 3);
+		// TODO: MAke sure to construct profiledExtensionPID and profiledExtensionPID!!!
+
+		// Create Mech2s display of Arm.
 		// the mechanism root node
+		Mechanism2d mech = new Mechanism2d(3, 3);
 		MechanismRoot2d root = mech.getRoot("climber", 1, 0);
 		var m_support = root.append(new MechanismLigament2d("support", 0.5, 90, 6, new Color8Bit(Color.kRed)));
 		m_wrist = m_support.append(new MechanismLigament2d("wrist", 0.5, 90, 6, new Color8Bit(Color.kPurple)));
 		m_elevator = m_wrist.append(new MechanismLigament2d("elevator", .5, 0));
 
 		SmartDashboard.putData("arm_info", mech);
-
-		profiledAnglePID.calculate(pitchEncoder.getPosition());
 
 	}
 
@@ -45,6 +54,7 @@ public class ArmSubsystem extends SubsystemBase {
 		armPitch.set(profiledAnglePID.calculate(pitchEncoder.getPosition()));
 		armExtension.set(profiledExtensionPID.calculate(extensionEncoder.getPosition()));
 
+		// Update Mech2d Display
 		m_wrist.setAngle(pitchEncoder.getPosition());
 		m_elevator.setLength(extensionEncoder.getPosition());
 	}
@@ -61,6 +71,47 @@ public class ArmSubsystem extends SubsystemBase {
 	// Sets the motor controlling arm length
 	public void setExtensionArmMotor(double targetDistanceMeters) {
 		profiledAnglePID.setGoal(new State(targetDistanceMeters, 0));
+
+	}
+
+	/**
+	 * Simulation Code
+	 */
+	private boolean simInit = false;
+	SingleJointedArmSim armsim;
+	ElevatorSim prismaticSim;
+	private RevEncoderSimWrapper pitchencsim;
+	private RevEncoderSimWrapper extensionencsim;
+
+	private void initSim() {
+		double armLengthMeters = 1;
+		double armMass = 2; // KG
+		double moa = SingleJointedArmSim.estimateMOI(armLengthMeters, armMass);
+		armsim = new SingleJointedArmSim(DCMotor.getNEO(1), 1, moa, armLengthMeters, Units.degreesToRadians(0),
+				Units.degreesToRadians(180), armMass, true);
+		prismaticSim = new ElevatorSim(DCMotor.getNEO(1), 1, 1, Units.inchesToMeters(3), 0, 1, false);
+		pitchencsim = RevEncoderSimWrapper.create(armPitch);
+		extensionencsim = RevEncoderSimWrapper.create(armExtension);
+	}
+
+	@Override
+	public void simulationPeriodic() {
+		if (!simInit) {
+			initSim();
+			simInit = true;
+		}
+		armsim.setInputVoltage(armPitch.get() * RobotController.getInputVoltage());
+		prismaticSim.setInputVoltage(armExtension.get() * RobotController.getInputVoltage());
+		armsim.update(Constants.kSimUpdateTime);
+		prismaticSim.update(Constants.kSimUpdateTime);
+		pitchencsim.setDistance(Units.radiansToDegrees(armsim.getAngleRads()));
+		pitchencsim.setVelocity(Units.radiansToDegrees(armsim.getVelocityRadPerSec())); // TODO should this be in rpm?
+
+		extensionencsim.setDistance(prismaticSim.getPositionMeters());
+		extensionencsim.setVelocity(prismaticSim.getVelocityMetersPerSecond()); // TODO should this be in rpm?
+
+		// TODO set encoder Positions!
+		// armsim.getAngleRads()
 
 	}
 
