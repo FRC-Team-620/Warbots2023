8f7a2be378e056c5931d354119db43e1533bf081
Luhan Wang <117863446+Lu-han-wang@users.noreply.github.com>
2023-02-27 17:47:47 -0500

    Update build.gradle

--- src/main/java/org/jmhsrobotics/frc2023/subsystems/Drivetrain.java
+++ src/main/java/org/jmhsrobotics/frc2023/subsystems/Drivetrain.java
@@ -438,6 +438,9 @@ public class Drivetrain extends SubsystemBase {
 
 	@Override
 	public void simulationPeriodic() {
+		if (this.commandedCurvature < 10)
+			return;
+
 		if (!simInit) {
 			initSim();
 			simInit = true;
